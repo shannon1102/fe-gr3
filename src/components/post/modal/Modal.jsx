0e4d9b3a10e3e7e8447a7486127f934221425d42
Duong Nguyen <duong.nguyen@dxlabs.com.au>
2022-07-29 19:08:17 +0700

    add modal

--- src/components/post/modal/Modal.jsx
+++ src/components/post/modal/Modal.jsx
@@ -1,30 +1,79 @@
-import React from 'react'
-import './modal.css'
-export default function Modal() {
+import React from "react";
+import Share from "../../share/Share";
+import { Cancel, CloseRounded, EmojiEmotions, Label, PermMedia, Room } from "@material-ui/icons";
+import "./modal.css";
+import { Button } from "@material-ui/core";
+export default function Modal({user,post,PF,desc,file}) {
   return (
-    <div className='modal-container'>
-      <div className='modal'>
-        {/* <h1>
-          This is a modal
-        </h1>
-        <p>Okieee</p> */}
-          <button>
-            Close me
+    <div className="modal-container">
+      <div className="modal">
+        <div className="modalHeader">
+          <h3>Edit post</h3>
+          <Button className="closeBtnModal" onClick={() => {}}>
+            <CloseRounded />
+          </Button>
+        </div>
+        <div className="modalBody">
+          <>
+          <div className="shareWrapper">
+        <div className="shareTop">
+          <img
+            className="shareProfileImg"
+            src={
+              user?.data?.avatar
+                ? user?.data?.avatar
+                : PF + "person/noAvatar.png"
+            }
+            alt=""
+          />
+          <input
+            placeholder={"What's in your mind " + user?.username + "?"}
+            className="shareInput"
+            ref={desc}
+          />
+        </div>
+        <hr className="shareHr" />
+        {file && (
+          <div className="shareImgContainer">
+            {(file.type.split("/")[0] === "image") && <img className="shareImg" src={URL.createObjectURL(file)} alt="" />}
+            {(file.type.split("/")[0] === "video") &&  <video width="750" height="500" controls ><source src={URL.createObjectURL(file)} type="video/mp4"/></video>}
+            <Cancel className="shareCancelImg" onClick={() => {}} />
+          </div>
+        )}
+        <form className="shareBottom" onSubmit={()=>{}}>
+          <div className="shareOptions">
+            <label htmlFor="file" className="shareOption">
+              <PermMedia htmlColor="tomato" className="shareIcon" />
+              <span className="shareOptionText">Photo or Video</span>
+              <input
+                style={{ display: "none" }}
+                type="file"
+                id="file"
+                accept=".png,.jpeg,.jpg,.mp4"
+                onChange={(e) => {}}
+              />
+            </label>
+            <div className="shareOption">
+              <Label htmlColor="blue" className="shareIcon" />
+              <span className="shareOptionText">Tag</span>
+            </div>
+            <div className="shareOption">
+              <Room htmlColor="green" className="shareIcon" />
+              <span className="shareOptionText">Location</span>
+            </div>
+            <div className="shareOption">
+              <EmojiEmotions htmlColor="goldenrod" className="shareIcon" />
+              <span className="shareOptionText">Feelings</span>
+            </div>
+          </div>
+          <button className="shareButton" type="submit">
+            Share
           </button>
-          <hr/>
-          <button>
-            Close me
-          </button>
-          <hr/>
-          <button>
-            Close me
-          </button>
-          <hr/>
-          <button>
-            Close me
-          </button>
-          <hr/>
+        </form>
+      </div>
+          </>
+        </div>
       </div>
     </div>
-  )
+  );
 }
---
+++ src/pages/profile/editUserInfoModal/EditUserInfoModal.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function EditUserInfoModal() {
+  return (
+    <div className="modal-container">
+      <div className="modal">
+        <div className="modal-header">
+          <button onClick={()=>{}}
+          >
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <p>Hello, I'm a modal.</p>
+      </div>
+    </div>
+  );
+}
